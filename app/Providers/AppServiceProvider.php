09568ee390246c2e1dd690eb4ee7796a0796126b
jessemac <jessemac@fino.com>
2018-11-04 03:25:20 +0000

    管理后台 - 同意退款 - 支付宝退款

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,7 +28,7 @@ class AppServiceProvider extends ServiceProvider
         // 往服务容器中注入一个名为 alipay 的单例对象
         $this->app->singleton('alipay', function () {
             $config = config('pay.alipay');
-            $config['notify_url'] = 'http://requestbin.leo108.com/1d1zmaq1';
+            $config['notify_url'] = 'http://requestbin.fullcontact.com/1lf5dnh1';
             $config['return_url'] = route('payment.alipay.return');
 
             // 判断当前项目运行环境是否为线上环境
