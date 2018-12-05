4463fe2b1e6ed98a396f2857dd09752ff354dbd2
vagrant <vagrant@homestead>
2018-12-05 07:16:16 +0000

    众筹结束逻辑

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,9 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+
+        // 每一分钟执行一次
+        $schedule->command('cron:finish-crowdfunding')->everyMinute();
     }
 
     /**
